57f91df545048d03144c05f93894e85d42cf9795
iamashura <hackerz55org@gmail.com>
2025-12-17 14:32:14 +0100

    adding some funtions like api cals and js shit am lost btw

--- weather app/WA.js
+++ weather app/WA.js
@@ -5,24 +5,47 @@ const cityinput = document.querySelector(".cityinput");
 const card = document.querySelector(".card");
 const apiKey = "79b2d3fc51e0e8b12a6db2ec2b8c0ff5";
 
-weatherform.addEventListener("submit", (event) => {
+weatherform.addEventListener("submit", async (event) => {
   event.preventDefault();
 
   const city = cityinput.value;
 
   if (city) {
+    try {
+      const weatherData = await getWeatherData(city);
+      displayWeatherInfo(weatherData);
+    } catch (error) {
+      console.error(error);
+      displayError(error);
+    }
   } else {
     displayError("please enter city");
   }
 });
 
-async function GetWeatherdata(city) {}
+async function getWeatherData(city) {
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
-async function DisplayWeatherInfo(data) {}
+  const response = await fetch(apiUrl);
+
+  // console.log(response);
+  if (!response.ok) {
+    throw new Error("could not fetch weather data");
+  }
+
+  retrun await response.json();
+}
+
+async function displayWeatherInfo(data) {}
 
 async function GetWeatherEmoji(weatherId) {}
 
 async function displayError(message) {
-  const errorDisplay = document.createElement("p");
-  errorDisplay.textContent = message;
+  const errordisplay = document.createElement("p");
+  errordisplay.textContent = message;
+  errordisplay.classList.add("errordisplay");
+
+  card.textContent = "";
+  card.style.display = "flex";
+  card.appendChild(errordisplay);
 }
